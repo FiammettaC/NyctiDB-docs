1a37f125192d7fdd3bf9f6c09b69e6686bc78f65
FiammettaC <fiammettacaccavale@yahoo.it>
2026-01-06 11:22:46 +0100

    adding docs files

--- docs/conf.py
+++ docs/conf.py
@@ -27,3 +27,11 @@ exclude_patterns = []
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True, 
+    "github_user": "FiammettaC", 
+    "github_repo": "NyctiDB-sphinx", 
+    "github_version": "main", 
+    "conf_py_path": "/docs/", 
+}
